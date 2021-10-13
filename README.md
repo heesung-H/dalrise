1ae88f471e7c6e7d952461129f04dd856dc77212
JO <johyun0218@naver.com>
2021-10-13 17:27:30 +0900

    first init

---
+++ README.md
@@ -0,0 +1,12 @@
+Material Theme
+Material Icon Theme
+Prettier - Code formatter
+
+ESLint
+https://github.com/standard/standard/blob/master/docs/RULES-kokr.md
+
+https://github.com/tonsky/FiraCode 설치
+
+yarn
+관리자 권한 power shell
+Set-ExecutionPolicy RemoteSigned 실행
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+//import { createConnection } from 'typeorm';
+
+function test(a: string) {
+  console.log(a + 'test');
+}
+
+const main = async () => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+
+  app.listen(3001, () => {
+    console.log('SERVER RUNNING ON PORT 3001');
+
+    test('aaaa');
+  });
+};
+
+main().catch(err => {
+  console.log(err);
+});
